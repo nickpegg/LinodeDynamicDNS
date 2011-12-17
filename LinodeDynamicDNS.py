f01336c291ad3db77d260411180b8c03ab4202d2
Nick Pegg <nick@nickpegg.com>
2011-12-17 11:23:55 -0600

    Made get_ip(), started working on the main program

--- LinodeDynamicDNS.py
+++ LinodeDynamicDNS.py
@@ -29,11 +29,63 @@ exit("Hey, you! You didn't change any settings. How do you expect this script to
 # You should probably stop editing stuff here unless you know what you're doing...
 
 try:
-	import linode-python
+    from linode import api
 except:
 	exit("Doesn't look like you have the linode-python library installed.")
 
 import re
 import urllib2
 
+def get_ip():
+    needle = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    ip = None
+
+    try:
+        haystack = urllib2.urlopen(CHECK_URL).read()
+    except:
+        print("Unable to open check URL.")
+        exit(-1)
+
+    match = re.search(needle, haystack)
+    if match is not None:
+        ip = match.group(0)
+    else:
+        print("Unable to get IP from check URL.")
+        exit(-1)
+
+    return ip    
+
+def main():
+    # Find our domain record
+    domain_id = None
+    domain_name = None
+    resource_id = None
+
+    linode = api.Api(API_KEY)
+    for domain in linode.domain_list():
+        if domain['DOMAIN'] in hostname:
+            domain_id = domain['DOMAINID']
+            domain_name = domain['DOMAIN']
+    
+    if domain_id is None:
+        print("Unable to find the domain in your account. Is your API key correct?")
+        return -2
+
+    for resource in linode.domain_resource_list(domainid=domain_id):
+        print(resource)
+
+    return 0
+    
+    ip = get_ip()
+
+    # Is our record already up to date?
+
+    return 0
+
+    # Update the record
+    return 1
+    
+
+if __name__ == "__main__":
+    exit(main())
 
